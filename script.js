9335c187726f23d6c26d101c05d5afb0287ecf65
FERNANDO DE PAULA NIZER <97744494+FernandoNizer@users.noreply.github.com>
2023-01-15 12:27:10 -0300

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function mostrar(secao){
+var secao = document.querySelector("section")
+var menu = document.querySelector("button#menu")
+
+if (display = "none"){
+  document.querySelector("section").style.display = "block";
+}else if(display = "block") {
+  document.querySelector("section").style.display = "none";
+}
+
+
+}
